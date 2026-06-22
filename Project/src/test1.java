65d5440b5da40f73eeb7b1b1031c41e6b5d2bb15
Itkitt <itkittkun@gmail.com>
2026-06-23 04:11:58 +0800

    2nd test, removed test1 class, added test1 java

---
+++ Project/src/test1.java
@@ -0,0 +1,10 @@
+public class test1 {
+    public static void main(String[] args){
+
+        // This is a test file to test out GitHub and Java
+
+        System.out.println("Hello World!");
+
+    }
+    
+}
